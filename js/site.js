dcb851619ed78ceb37ab611c6f3f24836b64e683
hoangdung <lehoangdung9x@gmail.com>
2024-11-01 15:12:39 +0700

    laptop

--- js/site.js
+++ js/site.js
@@ -1,7 +1,30 @@
 $(function () {
+    var pdfWidth = 1200; // Thay bằng chiều rộng thực tế của PDF
+    var pdfHeight = 800; // Thay bằng chiều cao thực tế của PDF
+
+    var screenWidth = window.innerWidth;
+    var screenHeight = window.innerHeight;
+
+    var margin = 0.05; // 5% không gian ở đầu và cuối
+
+    var availableHeight = screenHeight - (screenHeight * margin * 2); // Chiều cao khả dụng sau khi để lại margin
+
+    var pdfRatio = pdfWidth / pdfHeight;
+    var screenRatio = screenWidth / screenHeight;
+
+    var flipbookWidth, flipbookHeight;
+
+    if (pdfRatio > screenRatio) {
+        flipbookWidth = screenWidth;
+        flipbookHeight = screenWidth / pdfRatio;
+    } else {
+        flipbookHeight = availableHeight;
+        flipbookWidth = availableHeight * pdfRatio;
+    }
+
     $("#flipbook").turn({
-        width: 800,
-        height: 600,
+        width: flipbookWidth,
+        height: flipbookHeight,
         autoCenter: true
     });
 });
